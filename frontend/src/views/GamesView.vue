5a71567a480c8f64193c5bc51dcb059fdc81aedc
Arnaud <arnaud.maitre@he-arc.ch>
2025-02-27 11:26:14 +0100

    Added basic vue to list games

---
+++ frontend/src/views/GamesView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const games = ref([])
+
+const fetchGames = async () => {
+  const res = await axios.get('http://localhost:8000/api/games/')
+  games.value = res.data
+}
+
+onMounted(fetchGames)
+</script>
+
+<template>
+  <h1>Games</h1>
+  <ul>
+    <li v-for="game in games">
+      {{ game.name }}
+
+      <button @click="console.log('play: ' + game.id)">Play</button>
+
+      <button @click="console.log('leaderboard: ' + game.id)">Leaderboard</button>
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
