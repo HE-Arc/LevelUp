65773817f323366f53d242054d3125e6f74bfe21
mael brandt <mael.brandt@he-arc.ch>
2025-03-20 14:41:15 +0100

    renamed tab to my account

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -22,7 +22,7 @@ const toggleMenu = () => {
           <RouterLink to="/" @click="toggleMenu">Home</RouterLink>
           <RouterLink to="/about" @click="toggleMenu">About</RouterLink>
           <RouterLink to="/games" @click="toggleMenu">Games</RouterLink>
-          <RouterLink to="/personal" @click="toggleMenu">My Score</RouterLink>
+          <RouterLink to="/personal" @click="toggleMenu">My Account</RouterLink>
         </nav>
       </div>
     </header>
