6913cd9d020931668825cd0a017d69b908c98472
mael brandt <mael.brandt@he-arc.ch>
2025-03-20 13:48:52 +0100

    fetch and display game

--- frontend/src/views/PersonalView.vue
+++ frontend/src/views/PersonalView.vue
@@ -1,17 +1,47 @@
-<script>
-import { getCSRFToken } from '../services/auth'
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useAuthStore } from '../services/auth'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
 import { API_BASE_URL } from '@/config'
+import { RouterLink } from 'vue-router'
+
+const games = ref([])
+const authStore = useAuthStore()
+const router = useRouter()
+
+const fetchGames = async () => {
+  const res = await axios.get(`${API_BASE_URL}/games/`)
+  games.value = res.data
+}
+
+onMounted(async () => {
+  await authStore.fetchUser()
+  console.log('User loaded:', authStore.user)
+  if (!authStore.user) {
+    router.push('/login')
+    return
+  }
+  fetchGames()
+})
+
 </script>
 
 <template>
   <div class="container">
-    <h2>personnal</h2>
-    <div>
-      <p>
-        Hello there!
-      </p>
-      <p>Here are your scores</p>
-    </div>
+    <h1>{{ authStore.user?.username }}</h1>
+    <p v-if="games.length === 0">No games to display</p>
+    <ul v-else>
+      <li v-for="game in games">
+        <h2>
+          {{ game.name }}
+        </h2>
+        <div>
+          score here
+        </div>
+      </li>
+    </ul>
+
   </div>
 </template>
 
